d8f1fcf57b8d70a130cf9c52c1e2d58afc0604ec
NinjaOfPhysics <62783943+Ninja-of-Physics@users.noreply.github.com>
2024-02-05 15:22:40 -0700

    Adding Text to pop up on the screen

--- Assets/Scripts/RoomMove.cs
+++ Assets/Scripts/RoomMove.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomMove : MonoBehaviour
 {
     public Vector2 cameraChange; // How much we'll change the camera offset
     public Vector3 playerChange; // How much to move the player after room change
     private CameraMovement playerCam;
+    
+    // Title Card variables
+    public bool needText;
+    public string placeName;
+    public GameObject text;
+    public Text placeText;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,6 +34,18 @@ public class RoomMove : MonoBehaviour
             playerCam.minPosition += cameraChange;
             playerCam.maxPosition += cameraChange;
             collision.transform.position += playerChange;
+
+            if (needText)
+            {
+                StartCoroutine(placeNameCo());
+            }
         }
+    }
+    private IEnumerator placeNameCo()
+    {
+        text.SetActive(true);
+        placeText.text = placeName;
+        yield return new WaitForSeconds(2f);
+        text.SetActive(false);
     }
 }
